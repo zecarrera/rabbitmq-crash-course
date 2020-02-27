c28bf2260320a7924f731bab685b2791eeed8a14
Jose Neto <jose.neto@bglgroup.co.uk>
2020-02-27 17:05:39 +0000

    Adds an acknowledge command for a given input

--- consumer.js
+++ consumer.js
@@ -10,8 +10,12 @@ async function connectConsumer() {
         const result = await channel.assertQueue("jobs"); //verifies you have a queue
         console.log("Waiting for messages...");
         channel.consume("jobs", message => {
-            const input = message.content.toString();
-            console.log(`Received job with input: ${JSON.parse(input).number}`);
+            const input = JSON.parse(message.content.toString());
+            console.log(`Received job with input: ${input.number}`);
+            if (input.number == 10) {
+                channel.ack(message);
+                console.log(`Aknowledge message with input 10`);
+            }
         });
 
     } catch (e) {
